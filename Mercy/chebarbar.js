65cb733bb66781e4484b3c33f172fb4d51dff98d
AyiembaPearl <dorothyayiemba@gmail.com>
2024-11-19 14:05:30 +0300

    push to github

---
+++ Mercy/chebarbar.js
@@ -0,0 +1,5 @@
+document.getElementById('surpriseBtn').addEventListener('click', function() {
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    surpriseMessage.innerHTML = "🎈 You're the best, Chebarbar! I love you always and forever. Have always enjoyed your company and friendship, truly wish it never ends and I hope we have more and more funfilled lives. Sometimes I find you very secretive and challenging friend to have and that intrigues me to the core, it makes me want to know you more and keep up in a good way of course. Regardless, have a fantastic birthday filled with love and laughter! 🎈";
+  });
+  
